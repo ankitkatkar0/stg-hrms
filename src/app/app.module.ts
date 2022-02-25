82193045691932304168538ffe01ed34dfb990f7
Ankit Jadhao <ankitjadhao02@gmail.com>
2022-02-25 09:28:34 +0530

    sidebar menu btn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import {
   AgmCoreModule
 } from '@agm/core';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   imports: [
@@ -39,6 +40,7 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
   declarations: [
     AppComponent,
     AdminLayoutComponent,
+    MenuComponent,
 
   ],
   providers: [],
