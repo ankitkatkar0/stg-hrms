d6410c3d715eec33a5e99bb14718fb6c9abb88e3
ankit katkar <ankitkatkar8@gmail.com>
2022-03-06 05:03:21 -0800

    deploy hrms app in heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/hrms-material-dashboard-angular'));
+app.use(express.static('./dist/material-dashboard-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/hrms-material-dashboard-angular/'}),
+    res.sendFile('index.html', {root: 'dist/material-dashboard-angular/'}),
 );
 
 // Start the app by listening on the default Heroku port
